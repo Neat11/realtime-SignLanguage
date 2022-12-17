63a09f0ec0eca26bd2f45120f6752d34af0cba92
Neat11 <hello.astitva@gmail.com>
2022-12-17 23:25:38 +0530

    mandatory commit

--- LanguageRecognition.py
+++ LanguageRecognition.py
@@ -1,27 +1,12 @@
 import cv2
 import numpy as np
-import os
 from matplotlib import pyplot as plt
 import mediapipe as mp
 import pyvirtualcam
 
-mp_holistic = mp.solutions.holistic #bringing a holistic model
-mp_drawing = mp.solutions.drawing_utils #drawing utilities
 class LanguageRecognition:
-
-    def addString(self,string):
-        fmt = pyvirtualcam.PixelFormat.BGR
-        with pyvirtualcam.Camera(width=1280, height=720, fps=20, fmt=fmt) as cam:
-            ret_val,frame = cv2.VideoCapture(0).read()
-            x,y = 640-len(string)*10,670
-            frame = cv2.resize(frame, (1280, 720), interpolation=cv2.BORDER_DEFAULT)
-            cv2.putText(frame,f"{len(string)},{y}",(50,50),3,1,(0,0,0),1,cv2.LINE_8)
-            cv2.putText(frame,string,(x,y),3,1,(0,0,0),1,cv2.LINE_8)
-            frame = cv2.flip(frame,1)
-            cam.send(frame)
-            cam.sleep_until_next_frame()
-        cv2.destroyAllWindows()
-
+    mp_holistic = mp.solutions.holistic
+    mp_drawing = mp.solutions.drawing_utils #drawing utilities
     def mediapipe_detection(self,image,model):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
@@ -32,17 +17,17 @@ class LanguageRecognition:
 
     def draw_landmarks(self,image, results):
         if(results.left_hand_landmarks):
-            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            self.mp_drawing.draw_landmarks(image, results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS)
         if(results.right_hand_landmarks):
-            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
-        #                          mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-        #                          mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+            self.mp_drawing.draw_landmarks(image, results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS)
+        # self.mp_drawing.draw_landmarks(image, results.face_landmarks, self.mp_holistic.FACE_CONNECTIONS, 
+        #                          self.mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+        #                          self.mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
         #                          ) 
         # Draw pose connections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+        self.mp_drawing.draw_landmarks(image, results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS,
+                                self.mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                                self.mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
                                 ) 
     def extract_keypoints(self,results):
         lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
@@ -50,31 +35,34 @@ class LanguageRecognition:
         pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
         return np.concatenate([pose,lh, rh])
 
-    def __init__(self) -> None:
+    def startCapture(self) -> None:
         frameno = 0
         cap = cv2.VideoCapture(0)
         fmt = pyvirtualcam.PixelFormat.BGR
         with pyvirtualcam.Camera(width=1280, height=720, fps=20, fmt=fmt) as cam:
-            with mp_holistic.Holistic(min_detection_confidence =0.5, min_tracking_confidence =0.5) as holistic:        
+            with self.mp_holistic.Holistic(min_detection_confidence =0.5, min_tracking_confidence =0.5) as holistic:        
                 while (cap.isOpened()):
                     frameno+=1
                     ret, frame =cap.read()
                     image, results = self.mediapipe_detection(frame, holistic)
                     image = cv2.resize(image, (1280, 720), interpolation=cv2.BORDER_DEFAULT)
                     self.draw_landmarks(image, results)
-                    cv2.imshow("",image)
                     string = f"{frameno}"
                     x,y = 640-len(string)*10,670
-                    frame = cv2.resize(frame, (1280, 720), interpolation=cv2.BORDER_DEFAULT)
-                    cv2.putText(frame,f"{len(string)},{y}",(50,50),3,1,(0,0,0),1,cv2.LINE_8)
-                    cv2.rectangle(frame,(x,y+1),(x+len(string)*21,y-22),(0,0,0),-1)
-                    cv2.putText(frame,string,(x,y),3,1,(255,255,255),1,cv2.LINE_8)
+                    #frame = cv2.resize(frame, (1280, 720), interpolation=cv2.BORDER_DEFAULT)
+                    #cv2.putText(frame,f"{len(string)},{y}",(50,50),3,1,(0,0,0),1,cv2.LINE_8)
+                    #cv2.rectangle(frame,(x,y+1),(x+len(string)*21,y-22),(0,0,0),-1)
+                    #cv2.putText(frame,string,(x,y),3,1,(255,255,255),1,cv2.LINE_8)
+                    cv2.rectangle(image,(x,y+1),(x+len(string)*21,y-22),(0,0,0),-1)
+                    cv2.putText(image,string,(x,y),3,1,(255,255,255),1,cv2.LINE_8)
                     #frame = cv2.flip(frame,1)
-                    cam.send(frame)
+                    cam.send(image)
                     cam.sleep_until_next_frame()
-                    if(cv2.waitKey(10) and 0xFF == ord('q')):
+                    if(cv2.waitKey(5)%256==27):
                             break
-        cap.release()
+            cap.release()
         cv2.destroyAllWindows()
+    def __init__(self) -> None:
+        pass
 
 obj = LanguageRecognition()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import kivy 
+kivy.require('1.9.0')
+from kivy.core.window import Window
+Window.size = (600, 400)
 from kivy.app import App
-import kivy
 from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -8,12 +11,25 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+import os
+from LanguageRecognition import LanguageRecognition
 
 
+class Launch(Screen):
+    obj = LanguageRecognition()
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        print("launch initiated")
+
+    def launch_button(self):
+        print("launch_button intiated")
 
 class Splash(Screen):
-    pass
-
+    obj = LanguageRecognition()
+    def launch(self):
+        self.manager.current = 'Launch'
+        print("kuch ho gaya")
+        self.obj.startCapture()
 class MyScreenManager(ScreenManager):
     def changescreen(self, value):
         self.manager.current = value
