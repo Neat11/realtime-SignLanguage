1274485e582f35365d28885e3766cb6c38cc7acb
Chandan <chandancvp08@gmail.com>
2023-03-04 18:44:56 +0530

    removed pose action

--- v1.0.0/actions.py
+++ v1.0.0/actions.py
@@ -21,19 +21,10 @@ def draw_landmarks(image, results):
             mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
         if(results.right_hand_landmarks):
             mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                )
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                                 mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                                 ) 
 def extract_keypoints(results):
         lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
         rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-        return np.concatenate([pose,lh, rh])
+        return np.concatenate([lh, rh])
 
 with mp_holistic.Holistic(min_detection_confidence =0.5, min_tracking_confidence =0.5) as holistic:
     for action in actions:
