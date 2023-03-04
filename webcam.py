9f7e8a5cab1aa575120872ef61f4b9c18b0c7da3
Neat11 <hello.astitva@gmail.com>
2023-03-05 02:12:41 +0530

    added the training data and updated actionsArray

--- webcam.py
+++ webcam.py
@@ -1,10 +1,8 @@
 import numpy as np
-import createfiles
 # actions =np.array(['hello',"iLoveYou",'okay', 'helps', 'please', 'thankyou','play'])
 
 # np.save("actionsArray.npy", actions )
 
 
-createfiles.createUserFile("lmaooo")
 actions = np.load('actionsArray.npy')
 print(actions)
